a8e61b47d9b915910da0781ae46a913782d33b5d
l3oss123456 <46367212+l3oss123456@users.noreply.github.com>
2020-06-03 18:28:58 +0700

    Added a Procfile.

---
+++ src/index.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const line = require("@line/bot-sdk");
+require("dotenv/config");
+
+const app = express();
+// const config = require("./config")
+
+const PORT = 4800;
+
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
+};
+
+const client = new line.Client(config);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+function handleEvent(event) {
+  console.log(event);
+  if (event.type === "message" && event.message.type === "text") {
+    handleMessageEvent(event);
+  } else {
+    return Promise.resolve(null);
+  }
+}
+
+function handleMessageEvent(event) {
+  var msg = {
+    type: "text",
+    text: "สวัสดีครับ",
+  };
+
+  return client.replyMessage(event.replyToken, msg);
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`);
+});
+
+module.exports = app;
