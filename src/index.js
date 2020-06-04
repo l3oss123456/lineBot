bbf68558ddb0ff3984d514512c185a44f3ad7277
l3oss123456 <46367212+l3oss123456@users.noreply.github.com>
2020-06-04 15:56:15 +0700

    test deploy

--- src/index.js
+++ src/index.js
@@ -1,71 +1,71 @@
 
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-const PORT = process.env.PORT || 4000
+// const PORT = process.env.PORT || 4000
 
-app.get('/', (req, res) => {
-  res.json({
-    message: 'OK'
-  })
-})
+// app.get('/', (req, res) => {
+//   res.json({
+//     message: 'OK'
+//   })
+// })
 
-app.get('*', (req, res) => {
-  res.json({
-    message: 'Error'
-  })
-})
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on ${PORT}`)
-})
-
-// const express = require("express");
-// const line = require("@line/bot-sdk");
-// require("dotenv/config");
-
-// const app = express();
-// // const config = require("./config")
-
-// const PORT = 4800;
-
-// const config = {
-//   channelAccessToken: process.env.channelAccessToken,
-//   channelSecret: process.env.channelSecret,
-// };
-
-// const client = new line.Client(config);
-
-// app.get("/", (req, res) => {
-//   res.send("Hello World");
-// });
-
-// app.post("/webhook", line.middleware(config), (req, res) => {
-//   Promise.all(req.body.events.map(handleEvent)).then((result) =>
-//     res.json(result)
-//   );
-// });
-
-// function handleEvent(event) {
-//   console.log(event);
-//   if (event.type === "message" && event.message.type === "text") {
-//     handleMessageEvent(event);
-//   } else {
-//     return Promise.resolve(null);
-//   }
-// }
-
-// function handleMessageEvent(event) {
-//   var msg = {
-//     type: "text",
-//     text: "สวัสดีนะครับ",
-//   };
-
-//   return client.replyMessage(event.replyToken, msg);
-// }
+// app.get('*', (req, res) => {
+//   res.json({
+//     message: 'Error'
+//   })
+// })
 
 // app.listen(PORT, () => {
-//   console.log(`Server is running on port : ${PORT}`);
-// });
+//   console.log(`Server is listening on ${PORT}`)
+// })
 
-// module.exports = app;
+const express = require("express");
+const line = require("@line/bot-sdk");
+require("dotenv/config");
+
+const app = express();
+// const config = require("./config")
+
+const PORT = 4800;
+
+const config = {
+  channelAccessToken: process.env.channelAccessToken,
+  channelSecret: process.env.channelSecret,
+};
+
+const client = new line.Client(config);
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.post("/webhook", line.middleware(config), (req, res) => {
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
+
+function handleEvent(event) {
+  console.log(event);
+  if (event.type === "message" && event.message.type === "text") {
+    handleMessageEvent(event);
+  } else {
+    return Promise.resolve(null);
+  }
+}
+
+function handleMessageEvent(event) {
+  var msg = {
+    type: "text",
+    text: "สวัสดีนะครับ",
+  };
+
+  return client.replyMessage(event.replyToken, msg);
+}
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port : ${PORT}`);
+});
+
+module.exports = app;
