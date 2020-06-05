83aa268b5bed68a70b702f5fee0733a9054e65f9
l3oss123456 <46367212+l3oss123456@users.noreply.github.com>
2020-06-05 16:34:07 +0700

    test asychronous2

--- src/index.js
+++ src/index.js
@@ -88,20 +88,20 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/webhook", line.middleware(config), async (req, res) => {
-  await req.body.events.map((event) => {
-    if (event.type === "message" && event.message.type === "text") {
-      handleMessageEvent(event);
-    }
-  });
-  // Promise.all(
-  //   req.body.events.map((event) => {
-  //     if (event.type === "message" && event.message.type === "text") {
-  //       handleMessageEvent(event);
-  //     } else {
-  //       return Promise.resolve(null);
-  //     }
-  //   })
-  // ).then((result) => res.json(result));
+  // await req.body.events.map((event) => {
+  //   if (event.type === "message" && event.message.type === "text") {
+  //     handleMessageEvent(event);
+  //   }
+  // });
+  Promise.all(
+    req.body.events.map((event) => {
+      if (event.type === "message" && event.message.type === "text") {
+        handleMessageEvent(event);
+      } else {
+        return Promise.resolve(null);
+      }
+    })
+  ).then((result) => res.json(result));
 });
 
 const handleMessageEvent = async (event) => {
