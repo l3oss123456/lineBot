6880378e225ece03ef76c83fe019d683141279f7
l3oss123456 <46367212+l3oss123456@users.noreply.github.com>
2020-06-05 16:59:17 +0700

    test calMon6

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,9 @@
-require('dotenv/config');
+require("dotenv/config");
 
 module.exports = {
-    port: process.env.PORT || 8000
-}
+  port: process.env.PORT || 8000,
+  lineConfig: {
+    channelAccessToken: process.env.channelAccessToken,
+    channelSecret: process.env.channelSecret,
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,66 +1,3 @@
-// const express = require("express");
-// const line = require("@line/bot-sdk");
-// const bodyParser = require('body-parser')
-// const request = require('request')
-// require("dotenv/config");
-// const app = express();
-
-// const PORT = process.env.PORT || 4000;
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
-
-// // const client = new line.Client({
-// //   channelAccessToken: process.env.channelAccessToken
-// // });
-
-// app.get("/", (req, res) => {
-//   res.json({
-//     message: "Welcome to Nongbot",
-//   });
-// });
-
-// app.post("/webhook", (req, res) => {
-//   let reply_token = req.body.events[0].replyToken;
-//   reply(reply_token);
-//   res.sendStatus(200);
-// });
-
-// function reply(reply_token) {
-// let headers = {
-//   "Content-Type": "application/json",
-//   Authorization: `Bearer ${process.env.channelAccessToken}`,
-// };
-// let body = JSON.stringify({
-//   replyToken: reply_token,
-//   messages: [
-//     {
-//       type: "text",
-//       text: "Hello",
-//     },
-//     {
-//       type: "text",
-//       text: "How are you?",
-//     },
-//   ],
-// });
-// request.post(
-//   {
-//     url: "https://api.line.me/v2/bot/message/reply",
-//     headers: headers,
-//     body: body,
-//   },
-//   (err, res, body) => {
-//     console.log("status = " + res.statusCode);
-//   }
-// );
-// }
-
-// app.listen(PORT, () => {
-//   console.log(`Server is listening on ${PORT}`);
-// });
-
-// module.exports = app
-
 const express = require("express");
 const line = require("@line/bot-sdk");
 const axios = require("axios");
@@ -68,14 +5,9 @@ const CircularJSON = require("circular-json");
 require("dotenv/config");
 
 const app = express();
-// const config = require("./config")
+const config = require("./config")
 
-const PORT = process.env.PORT || 4000;
-
-const config = {
-  channelAccessToken: process.env.channelAccessToken,
-  channelSecret: process.env.channelSecret,
-};
+const PORT = config.port
 
 const client = new line.Client({
   channelAccessToken: process.env.channelAccessToken,
@@ -85,23 +17,9 @@ app.get("/", async (req, res) => {
   res.json({
     message: "Welcome to nongBot",
   });
-  // const resp = await axios.get(
-  //   `http://data.fixer.io/api/latest?access_key=${process.env.fixerApiKey}`
-  // );
-  // const json = CircularJSON.stringify(resp);
-  // res.send(JSON.parse(json));
-  // const cal =
-  //   (100 / JSON.parse(json).data.rates.USD) *
-  //   parseFloat(JSON.parse(json).data.rates.THB);
-  // console.log("cal: ", cal);
 });
 
-app.post("/webhook", line.middleware(config), async (req, res) => {
-  // await req.body.events.map((event) => {
-  //   if (event.type === "message" && event.message.type === "text") {
-  //     handleMessageEvent(event);
-  //   }
-  // });
+app.post("/webhook", line.middleware(config.lineConfig), (req, res) => {
   Promise.all(
     req.body.events.map((event) => {
       if (event.type === "message" && event.message.type === "text") {
@@ -122,10 +40,6 @@ const handleMessageEvent = async (event) => {
     (parseFloat(event.message.text) /
       parseFloat(JSON.parse(json).data.rates.USD)) *
     parseFloat(JSON.parse(json).data.rates.THB);
-  // var msg = {
-  //   type: "text",
-  //   text: event.message.text,
-  // };
   var msg = {
     type: "text",
     text: cal.toFixed(2),
